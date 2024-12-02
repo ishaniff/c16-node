e22015210e815999155047aa70c979590b20ab8f
ishaniff <167576494+ishaniff@users.noreply.github.com>
2024-12-02 17:23:48 -0500

    section 3 episode 12

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,9 @@
-// const add = require("./utils.js");
-// const sum = add(4, -2);
-// console.log(sum);
-
-// challenge, define and use a function in a new file, new file called notes.js, create getNotes function that returns "your notes...", export getNotes function, from app.js load in and call function printing message to console
-
+const validator = require("validator");
 const getNotes = require("./notes.js");
 console.log(getNotes());
+
+// use chalk, install chalk v 2.4.1, load chalk, print 'success' in green
+
+const chalk = require("chalk");
+const log = console.log;
+log(chalk.green.bgBlue.bold("success"));
--- notes-app/utils.js
+++
@@ -1,6 +0,0 @@
-// console.log("utils.js");
-// const name = "issiah";
-// const add = function (a, b) {
-//   return a + b;
-// };
-// module.exports = add;
