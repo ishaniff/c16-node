d39b83168d9fb541699af59eeaa41a9d2135a18d
ishaniff <167576494+ishaniff@users.noreply.github.com>
2024-12-02 12:35:11 -0500

    section 3 epioode 10

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
-const FileSystem = require("fs");
-// FileSystem.writeFileSync("notes.txt", "my name is issiah");
+// const add = require("./utils.js");
+// const sum = add(4, -2);
+// console.log(sum);
 
-// append msg to notes.txt, use appendFileSync
-FileSystem.appendFileSync("notes.txt", " i am learning node");
+// challenge, define and use a function in a new file, new file called notes.js, create getNotes function that returns "your notes...", export getNotes function, from app.js load in and call function printing message to console
+
+const getNotes = require("./notes.js");
+console.log(getNotes());
---
+++ notes-app/utils.js
@@ -0,0 +1,6 @@
+// console.log("utils.js");
+// const name = "issiah";
+// const add = function (a, b) {
+//   return a + b;
+// };
+// module.exports = add;
